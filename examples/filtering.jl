003ec4694b983a9788d050e3ebf808dd791aabae
Jay Kickliter <jay@kickliter.com>
2014-09-02 17:59:03 -0600

    Including both highpass and lowpass examples

--- examples/filtering.jl
+++ examples/filtering.jl
@@ -1,4 +1,4 @@
-reload( "/Users/jaykickliter/.julia/v0.3/AudioIO/src/AudioIO.jl" )
+reload( expanduser( "~/.julia/v0.3/AudioIO/src/AudioIO.jl" ) )
 import AudioIO
 
 
@@ -6,23 +6,33 @@ import AudioIO
 AudioIO.play([0])
 AudioIO.sleep(2)
 
-tones        = AudioIO.SinOsc(440) + AudioIO.SinOsc(8000)
-filterdTones = AudioIO.Filt( tones, 5000 )
+tones = AudioIO.SinOsc(440) + AudioIO.SinOsc( 7040 )
 
-# Now play the filtered audio
-AudioIO.play( filterdTones )
-AudioIO.sleep( 1 )
-AudioIO.stop( filterdTones )
 
-# Sleep to check for transition
-AudioIO.sleep( 0.25 )
-
-# Make sure I have it set up correctly
+println( "Playing both tones")
 AudioIO.play( tones )
-AudioIO.sleep( 1 )
+AudioIO.sleep( 2 )
 AudioIO.stop( tones )
 
+# Sleep to check for transition
+AudioIO.sleep( 0.5 )
 
+# seem to have to redefine tones to get the filtering to work after previously playing the unfiltered tones
+tones     = AudioIO.SinOsc( 440 ) + AudioIO.SinOsc( 7040 )
+lowpassed = AudioIO.Filt( tones, 4e3 )
 
+println( "Playing lowpass filterned tones" )
+AudioIO.play( lowpassed )
+AudioIO.sleep( 4 )
+AudioIO.stop( lowpassed )
 
+# Sleep to check for transition
+AudioIO.sleep( 0.5 )
 
+tones      = AudioIO.SinOsc( 440 ) + AudioIO.SinOsc( 7040 )
+highpassed = AudioIO.Filt( tones, 4e3, response = HIGHPASS )
+
+println( "Playing highpass filterned tones" )
+AudioIO.play( highpassed )
+AudioIO.sleep( 4 )
+AudioIO.stop( highpassed )
