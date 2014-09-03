477e13f281d6d27792b76c94493c3a9302e01417
Jay Kickliter <jay@kickliter.com>
2014-09-03 13:59:16 -0600

    Changed to WAV filtering example to immediatly transition from normal -> lowpass -> highpass.
    Still can't figure out a way to just read a file and switch filters to get a seamless transition between the parts

--- examples/WAV Filtering.jl
+++ examples/WAV Filtering.jl
@@ -8,17 +8,18 @@ part1 = ArrayPlayer( read( file, 44100*10, Float32 ) )
 part2 = ArrayPlayer( read( file, 44100*10, Float32 ) )
 part3 = ArrayPlayer( read( file, 44100*10, Float32 ) )
 
+lowpassed  = Filt( part2, 1e3 )
+highpassed = Filt( part3, 1e3, response = HIGHPASS )
 
 play( part1 )
 sleep(10)
-stop( part1 )
 
-lowpassed = Filt( part2, 4e3 )
 play( lowpassed )
+stop( part1 )
 sleep( 10 )
-stop( lowpassed )
 
-highpassed = Filt( part3, 4e3, response = HIGHPASS )
 play( highpassed )
+stop( lowpassed )
 sleep( 10 )
+
 stop( highpassed )
